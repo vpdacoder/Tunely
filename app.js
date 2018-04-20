6cea93017d3ebe8d815ee3ea5a02344ce666cb06
vpdacoder <vaipatel@utexas.edu>
2018-04-19 20:23:43 -0500

    Initial Template is Complete

---
+++ app.js
@@ -0,0 +1,21 @@
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('error');
+});
+
+app.listen(3000);
+console.log('3000 is the magic port');
