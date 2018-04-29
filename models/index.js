d73e6f6cf34d35db697de90ebf407d1263a6a47b
vpdacoder <vaipatel@utexas.edu>
2018-04-29 12:12:59 -0500

    Connected mLab

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/tunely");
+// mongoose.connect("mongodb://localhost/tunely");
+mongoose.connect("mongodb://vpdacoder:password@ds127564.mlab.com:27564/tunely");
+
 
 module.exports.Album = require("./albums");
