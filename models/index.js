2ce0f7b17a395b1cc582fed04725083371df316c
vpdacoder <vaipatel@utexas.edu>
2018-04-29 14:05:56 -0500

    Added Environmental Variable

--- models/index.js
+++ models/index.js
@@ -1,6 +1,10 @@
 var mongoose = require("mongoose");
+
+var url = process.env.DATABASEURL || "mongodb://localhost/tunely"
+mongoose.connect(url);
+// console.log(process.env.DATABASEURL);
 // mongoose.connect("mongodb://localhost/tunely");
-mongoose.connect("mongodb://vpdacoder:password@ds127564.mlab.com:27564/tunely");
+// mongoose.connect("mongodb://vpdacoder:password@ds127564.mlab.com:27564/tunely");
 
 
 module.exports.Album = require("./albums");
